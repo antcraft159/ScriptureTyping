59bb231b743a4dcdebdfbdbb5f6428ca0db6a183
antcraft159 <manofgod010802@gmail.com>
2026-03-10 11:25:12 +0900

    fix:ClozeGameViewModel.cs 분리

--- ViewModels/Games/Cloze/Modes/SamuelRank1/ClozeGameViewModel.SamuelRank1.cs
+++ ViewModels/Games/Cloze/Modes/SamuelRank1/ClozeGameViewModel.SamuelRank1.cs
@@ -10,6 +10,41 @@ namespace ScriptureTyping.ViewModels.Games
         private bool IsSamuelRank1Mode =>
             string.Equals(CurrentDifficulty, DIFFICULTY_SAMUEL_RANK1, StringComparison.Ordinal);
 
+        private int GetSamuelRank1BlankCount()
+        {
+            return 2;
+        }
+
+        private int GetSamuelRank1ChoiceCount()
+        {
+            return 4;
+        }
+
+        private int GetSamuelRank1TryCount()
+        {
+            return 1;
+        }
+
+        private int GetSamuelRank1CorrectScore()
+        {
+            return 20;
+        }
+
+        private int GetSamuelRank1WrongPenalty()
+        {
+            return 5;
+        }
+
+        private bool IsSamuelRank1TimeAttack()
+        {
+            return true;
+        }
+
+        private int GetSamuelRank1TimeAttackSeconds()
+        {
+            return 8;
+        }
+
         private async Task<bool> RunSamuelRank1PreviewAsync(int localVersion)
         {
             if (_current == null)
---
+++ ViewModels/Games/Cloze/Modes/VeryHard/ClozeGameViewModel.VeryHard.cs
@@ -0,0 +1,45 @@
+﻿namespace ScriptureTyping.ViewModels.Games
+{
+    public sealed partial class ClozeGameViewModel
+    {
+        private bool IsVeryHardDifficulty()
+        {
+            return CurrentDifficulty == DIFFICULTY_VERY_HARD;
+        }
+
+        private int GetVeryHardBlankCount()
+        {
+            return 2;
+        }
+
+        private int GetVeryHardChoiceCount()
+        {
+            return 5;
+        }
+
+        private int GetVeryHardTryCount()
+        {
+            return 1;
+        }
+
+        private int GetVeryHardCorrectScore()
+        {
+            return 16;
+        }
+
+        private int GetVeryHardWrongPenalty()
+        {
+            return 4;
+        }
+
+        private bool IsVeryHardTimeAttack()
+        {
+            return true;
+        }
+
+        private int GetVeryHardTimeAttackSeconds()
+        {
+            return 10;
+        }
+    }
+}
